80c9c7a038a0667f85b46fb28fd918253607ac42
Neel Parikh <nkp.parikh@gmail.com>
2015-03-15 14:24:32 -0600

    methods description

---
+++ memchunk.h
@@ -0,0 +1,27 @@
+/*
+ * Student: Neel Parikh
+ * Id: 1358644
+ * Cmput 379 Assignment 1
+ */
+
+#ifndef _MEMCHUNKH_
+#define _MEMCHUNKH_
+/* chunk information */
+struct memchunk {
+    void *start;
+    unsigned long length;
+    int RW;
+};
+
+/* handle segmentation fault */
+void signal_handler(int signo);
+
+/* returns number of chunks in process memory */
+int get_mem_layout(struct memchunk *chunk_list, int size);
+
+/* given an addresss, checks its accesibility*/
+int checkRW(char *ptr);
+
+
+
+#endif
